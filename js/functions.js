2f9657d0a249a6b92af6278879340741aab9a7a8
Makar Rybkin <makarrybkin@gmail.com>
2024-09-25 20:51:04 +0500

    module2-task1

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+const checkMaxLength = (string,maxLength) => string.length <= maxLength;
+
+const isPalindrome = function(string){
+  let reversedString = string.replaceAll(' ','').toLowerCase().split('').reverse().join('')
+  return reversedString === string;
+};
+
